e1a8a44972c280ca13f791d0015c17f9b54b1e0d
AegisVP <vlad@pysarenko.com>
2023-01-16 21:35:11 +0100

    update constants

--- web/src/redux/constants.js
+++ web/src/redux/constants.js
@@ -1,40 +1,6 @@
-const initialUser = {
-  email: null,
-  subscription: 'basic',
-  isVerified: false,
-};
-
-const initialWeighing = {
-  date: Date.now(),
-  auto: {
-    id: null,
-    driver: '',
-  },
-  crop: {
-    name: '',
-    source: '',
-    destination: '',
-  },
-  weighing: {
-    tare: null,
-    brutto: null,
-    netto: null,
-    isIncoming: true,
-  },
-  harvesters: [],
-  addedBy: initialUser.email,
-};
-
-const initialStore = {
-  user: initialUser,
-  weighings: initialWeighing,
-};
-
 export const CONST = {
-  initialUser,
-  initialWeighing,
-  initialStore,
-  BASE_URL: 'https://vital-agro-api.onrender.com',
+  // BASE_URL: 'http://localhost:8888',
+  BASE_URL: 'https://vitalagro.onrender.com',
   icon: {
     clear: '❌',
     delete: '🗑',
