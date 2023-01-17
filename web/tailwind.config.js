a83366c6f1da3a19169164424ac621387f579c16
AegisVP <vlad@pysarenko.com>
2023-01-17 13:32:07 +0100

    install tailwind

---
+++ web/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./src/components/**/*.{css,html,jsx}', './src/pages/*.{css,html,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
