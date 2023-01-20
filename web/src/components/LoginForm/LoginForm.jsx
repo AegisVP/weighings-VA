58881f4bdfde90185958ee21b0b942ded072247b
AegisVP <vlad@pysarenko.com>
2023-01-21 00:47:40 +0100

    add login error message

--- web/src/components/LoginForm/LoginForm.jsx
+++ web/src/components/LoginForm/LoginForm.jsx
@@ -1,18 +1,22 @@
 import { Loader } from 'components/Loader/Loader';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectUserIsLoading } from 'redux/selectors';
+import { selectUserError, selectUserIsLoading } from 'redux/selectors';
 
 import { loginUser } from 'redux/user/userOperations';
-import { StyledButton, StyledEntryField, StyledForm, StyledInputField, StyledLabel } from './LoginForm.styled';
+import { ErrorMessage, StyledButton, StyledEntryField, StyledForm, StyledInputField, StyledLabel } from './LoginForm.styled';
 
 export const LoginForm = () => {
+  const [inputTouched, setInputTouched] = useState(false);
   const isLoading = useSelector(selectUserIsLoading);
+  const userError = useSelector(selectUserError);
   const dispatch = useDispatch();
 
   const submitHandler = e => {
     e.preventDefault();
     const { email, password } = e.currentTarget.elements;
 
+    setInputTouched(false);
     dispatch(loginUser({ email: email.value, password: password.value }));
   };
 
@@ -20,17 +24,19 @@ export const LoginForm = () => {
     <StyledForm onSubmit={submitHandler} autoComplete="off">
       <StyledEntryField>
         <StyledLabel>Username:</StyledLabel>
-        <StyledInputField type="text" name="email" autoComplete="username" required />
+        <StyledInputField type="text" name="email" autoComplete="username" onChange={() => setInputTouched(true)} required />
       </StyledEntryField>
 
       <StyledEntryField>
         <StyledLabel>Password:</StyledLabel>
-        <StyledInputField type="password" name="password" autoComplete="current-password" required />
+        <StyledInputField type="password" name="password" autoComplete="current-password" onChange={() => setInputTouched(true)} required />
       </StyledEntryField>
 
       <StyledButton type="submit" disabled={isLoading}>
         {isLoading ? <Loader style={{ width: '24px', height: '24px' }} /> : 'Вхід'}
       </StyledButton>
+
+      {!!userError && !inputTouched && <ErrorMessage>{userError}</ErrorMessage>}
     </StyledForm>
   );
 };
